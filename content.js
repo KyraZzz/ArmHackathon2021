aa8702d9f09ece2cae3bab4e46039f5317ac502c
KyraZzz <yz709@cam.ac.uk>
2021-07-22 23:40:59 +0100

    enable text highlighter on any webpage

--- content.js
+++ content.js
@@ -1,9 +1,29 @@
+var deselect = () => {
+    var elementList = document.getElementsByClassName("acronym-marker");
+    var selection = window.getSelection();
+    for (var i = 0; i < elementList.length;i++) {
+        var ele = elementList[i];
+        if (selection.toString() !== ele.textContent){
+            var newElement = document.createElement("span");
+            newElement.innerText = ele.textContent;
+            ele.replaceWith(newElement);
+        }
+    }
+}
+
 var listener = () => {
     var selection = window.getSelection();
+    var element = document.createElement("mark");
+    element.style.fontWeight = "bold";
+    element.style.color = "black";
+    element.className = "acronym-marker";
     if (selection.rangeCount > 0) {
         var range = selection.getRangeAt(0);
-        var text = range.cloneContents().textContent;
-        console.log(text);
+        range.surroundContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
     }
+    deselect();
+    
 };
 document.addEventListener('dblclick', listener);
